496e7aa261cb2d2e0b5c9f54d73a2ab88ecced81
hbader <hbader@brookgrp.com>
2020-11-08 13:38:49 +0200

    V2

--- app/src/main/java/edu/cs/birzeit/bmicalculator/Activity2.java
+++ app/src/main/java/edu/cs/birzeit/bmicalculator/Activity2.java
@@ -3,12 +3,72 @@ package edu.cs.birzeit.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class Activity2 extends AppCompatActivity {
+    EditText time;
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        time=findViewById(R.id.editTextTime);
+         seconds=Integer.valueOf(time.getText().toString());
+
+        if(savedInstanceState !=null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
+
+
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        bundle.putInt("seconds", seconds);
+        bundle.putBoolean("running", running);
+    }
+
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        seconds = Integer.valueOf(time.getText().toString());
+        running = false;
+
+    }
+    private void runTimer(){
+        final TextView txtView = (TextView) findViewById(R.id.textViewTime);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = seconds % 3600 /60;
+                int snds = seconds % 60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, snds);
+                txtView.setText(time);
+                if(running){
+                    --seconds;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
